20684d487d75e5f37c58725e27c0934e4a9c5e07
microprediction <info@3za.org>
2020-07-27 08:33:16 -0400

    version and MUID requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microconventions",
-    version="0.0.6",
+    version="0.0.7",
     description="Conventions used at MicroPrediction.Org",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["muid>=0.4.8","getjson","pymorton","numpy","scipy"],
+    install_requires=["muid>=0.5.0","getjson","pymorton","numpy","scipy"],
     entry_points={
         "console_scripts": [
             "microconventions=microconventions.__main__:main",

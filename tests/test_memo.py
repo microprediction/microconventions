cca23367e63c2960487af2d6a57f546f1d1c1c45
Peter Cotton <peter.cotton@microprediction.com>
2020-09-26 10:18:48 -0400

    Revert "urgh"
    This reverts commit a4730538a8a3569b587f1bc15c8f9312093c80dd.

--- tests/test_memo.py
+++ tests/test_memo.py
@@ -1,14 +1,7 @@
 from microconventions import Memo, Activity, Genre
-import sys
-
 
 def test_memo():
-    if sys.version_info.major>=3 and sys.version_info.minor>=7:
-        memo = Memo(activity=Activity.set,genre=Genre.repository, success=5, execution=1, data={'my_no':13})
-    else:
-        # <3.7 we need to supply all arguments
-        memo = Memo(activity=Activity.set, genre=Genre.repository, success=5, execution=1, data={'my_no':13},
-                    warned=0, recipient='nobody',sender='nobody',write_key='asdf')
+    memo = Memo(activity=Activity.set,genre=Genre.repository, success=5, execution=1, data={'my_no':13})
     d = memo.to_dict(flatten_data=True)
     assert 'my_no' in d
     assert 'data' not in d

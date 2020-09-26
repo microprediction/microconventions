db3f8a33d6cd72e619cfe79436f0d7d3bd2c3260
Peter Cotton <peter.cotton@microprediction.com>
2020-09-26 12:08:40 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microconventions",
-    version="0.1.0",
+    version="0.5.0",
     description="Conventions used at MicroPrediction.Org",
     long_description=README,
     long_description_content_type="text/markdown",

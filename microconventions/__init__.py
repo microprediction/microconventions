1a105f8f4e2fc1ee76ae90d8d83ab83378c87873
Peter Cotton <peter.cotton@microprediction.com>
2020-09-27 09:05:58 -0400

    __init__

--- microconventions/__init__.py
+++ microconventions/__init__.py
@@ -1,3 +1,5 @@
 from microconventions.url_conventions import API_URL, FAILOVER_API_URL, api_url, failover_api_url, get_config
 from microconventions.conventions import KeyConventions, StreamConventions, ValueConventions, MicroConventions
 from microconventions.key_conventions import new_key, create_key, maybe_create_key, animal_from_key, shash, animal_from_code, key_difficulty
+from microconventions.stats_conventions import nudged, is_discrete, evenly_spaced_percentiles, cdf_values,\
+    quantize, discrete_pdf, discrete_cdf, sign_changes, is_process, nudged

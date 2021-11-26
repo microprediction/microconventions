06498626e8056c4b8957d49b80a4df6bc5afc160
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-11-26 17:29:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["microconventions"],
     test_suite='pytest',
